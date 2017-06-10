75cfb023678a99b92fbe869ff3d9852c34eb6a8b
Welcome <Welcome@Welcome-PC>
2017-06-10 08:54:43 +0530

    commited

--- appPortal/WebContent/WEB-INF/pages/promocodegenerate.jsp
+++ appPortal/WebContent/WEB-INF/pages/promocodegenerate.jsp
@@ -4,7 +4,7 @@
 <%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
 
-<html>
+<htaml>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title>:: APP ::</title>
--- appPortal/src/com/iws/appportal/utils/AppCodeConstants.java
+++ appPortal/src/com/iws/appportal/utils/AppCodeConstants.java
@@ -5,4 +5,16 @@ public class AppCodeConstants {
 	public static String INSERT_QUERY=PropertiesUtility.getPropertiesValue("INSERT_CODES_QUERY");
 	
 	public static String SELECT_LIST_CAMPAIGNS=PropertiesUtility.getPropertiesValue("SELECT_LIST_CAMPAIGNS");
+	
+	public static String SELECT_LIST_PRODUCTS=PropertiesUtility.getPropertiesValue("SELECT_LIST_PRODUCTS");
+	
+	public static String SELECT_CAMPAIGN_KEY_SHORT_CODE_INFO=PropertiesUtility.getPropertiesValue("SELECT_CAMPAIGN_KEY_SHORT_CODE_INFO");
+	
+	public static String SELECT_COUNT_CAMP_PRODUCT=PropertiesUtility.getPropertiesValue("SELECT_COUNT_CAMP_PRODUCT");
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println(SELECT_CAMPAIGN_KEY_SHORT_CODE_INFO);
+	}
 }
